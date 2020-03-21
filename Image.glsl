cac2193b91525265ba37e8ab11d82549993e9e4e
Dmitry V. Sokolov <ha@haqr.eu>
2020-03-21 23:17:56 +0100

    step 8

--- Image.glsl
+++ Image.glsl
@@ -1,3 +1,13 @@
+#define BUNW 6
+#define BUNH 6
+#define BUND 4
+#define BUNVOXSIZE 0.1
+uint[] bunny_bitfield = uint[]( 0xc30d0418u, 0x37dff3e0u, 0x7df71e0cu, 0x004183c3u, 0x00000400u );
+bool bunny(in int cubeID) {
+    if (cubeID<0 || cubeID>=BUNW*BUNH*BUND) return false;
+    return 0u != (bunny_bitfield[cubeID/32] & (1u << (cubeID&31)));
+}
+
 #define JFIGW 32u
 #define JFIGH 18u
 uint[] jfig_bitfield = uint[]( 0x0u,0x0u,0x0u,0xf97800u,0x90900u,0xc91800u,0x890900u,0xf90900u,0x180u,0x0u,0x30e30e0u,0x4904900u,0x49e49e0u,0x4824820u,0x31e31e0u,0x0u,0x0u,0x0u );
@@ -34,7 +44,7 @@ bool box_ray_intersect(in Box box, in Ray ray, out vec3 point, out vec3 normal) 
         int i1 = (d+1)%3;
         int i2 = (d+2)%3;
         if (point[i1]>box.center[i1]-box.halfsize[i1] && point[i1]<box.center[i1]+box.halfsize[i1] &&
-            point[i2]>box.center[i2]-box.halfsize[i2] && point[i2]<box.center[i2]+box.halfsize[i2]) {
+                point[i2]>box.center[i2]-box.halfsize[i2] && point[i2]<box.center[i2]+box.halfsize[i2]) {
             normal = vec3(0);
             normal[d] = side;
             return true;
@@ -43,17 +53,37 @@ bool box_ray_intersect(in Box box, in Ray ray, out vec3 point, out vec3 normal) 
     return false;
 }
 
+bool bunny_ray_intersect(in Ray ray, out vec3 point, out vec3 normal) {
+    float bunny_dist = 1e10;
+    for (int i=0; i<BUNW; i++) {
+        for (int j=0; j<BUNH; j++) {
+            for (int k=0; k<BUND; k++) {
+                int cellID = i+j*BUNW+k*BUNW*BUNH;
+                if (!bunny(cellID)) continue;
+                Box box = Box(vec3(i-BUNW/2,j-BUNH/2,-k+BUND/2)*BUNVOXSIZE+vec3(.5,.5,-.5)*BUNVOXSIZE, vec3(1.,1.,1.)*BUNVOXSIZE*.45);
+                vec3 p, n;
+                if (box_ray_intersect(box, ray, p, n) && length(p-ray.origin) < bunny_dist) {
+                    bunny_dist = length(p-ray.origin);
+                    point = p;
+                    normal = n;
+                }
+            }
+        }
+    }
+    return bunny_dist < 1e3;
+}
+
 vec3 cast_ray(in Ray ray) {
     vec3 p, n;
-    if (box_ray_intersect(Box(vec3(0.), vec3(.25)), ray, p, n)) {
-        vec3 diffuse_light = vec3(0.);
-        for (int i=0; i<lights.length(); i++) {
-            vec3 light_dir = normalize(lights[i].position - p);
-            diffuse_light  += lights[i].color * max(0., dot(light_dir, n));
-        }
-        return vec3(0.2, 0.7, 0.8)*(vec3(.7,.7,.7) + diffuse_light);
+    if (!bunny_ray_intersect(ray, p, n))
+        return texture(iChannel0, ray.dir).xyz;
+
+    vec3 diffuse_light = vec3(0.);
+    for (int i=0; i<lights.length(); i++) {
+        vec3 light_dir = normalize(lights[i].position - p);
+        diffuse_light  += lights[i].color * max(0., dot(light_dir, n));
     }
-    return texture(iChannel0, ray.dir).xyz;
+    return vec3(0.2, 0.7, 0.8)*(vec3(.7,.7,.7) + diffuse_light);
 }
 
 vec3 rotateCamera(in vec3 orig, in vec3 dir, in vec3 target) {
